745af2d81a674636317e4ab19070e5250157e2e5
Igorr <portnowIgor86@yandex.ru>
2017-12-15 12:33:30 +0300

    15.12.17(1)

--- src/Agency/Estate/QuantityOfRooms.java
+++ src/Agency/Estate/QuantityOfRooms.java
@@ -1,11 +1,7 @@
 package Agency.Estate;
 
 public enum QuantityOfRooms {
-    ONE("1"), TWO,THREE, FOUR;
-    private String abbreviation;
-    private Size(String abbreviation)
-    { this.abbreviation = abbreviation; }
-    public String getAbbreviation() { return abbreviation; }
-}
+    ONE, TWO,THREE, FOUR
+    }
 
 
